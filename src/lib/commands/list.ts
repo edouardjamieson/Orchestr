f4b685d021580b2d90a67ce6fa4218c9356f93c4
Charles-Édouard Jamieson <edouardjamieson99@gmail.com>
2025-09-20 14:14:48 -0400

    Fixed error with the -l command when no .orchestr directory

--- src/lib/commands/list.ts
+++ src/lib/commands/list.ts
@@ -2,6 +2,8 @@ import chalk from 'chalk';
 import Utils from '../utils';
 import DefaultCommand from './commands.service';
 import { CommandOptions } from './commands.type';
+import path from 'path';
+import { existsSync } from 'fs';
 
 export class CommandList extends DefaultCommand {
   constructor(props: CommandOptions) {
@@ -11,6 +13,14 @@ export class CommandList extends DefaultCommand {
     Utils.clearConsole();
     console.log(DefaultCommand.title);
     console.log(chalk.gray(DefaultCommand.separator));
+
+    // Check if the .orchestr directory exists
+    if (!existsSync(path.join(process.cwd(), Utils.BASE_PATH))) {
+      Utils.logError(
+        'The .orchestr directory does not exist. Make sure to create a script first.'
+      );
+      process.exit(1);
+    }
 
     // List scripts
     const scripts = this.listScripts();
