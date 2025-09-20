4b54a4237c6e63e3ddceedb567e941b55c429210
Charles-Édouard Jamieson <edouardjamieson99@gmail.com>
2025-09-20 14:10:23 -0400

    Update CLI program name and descriptions for improved clarity and branding

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import { CommandList, CommandRun, CommandValidate } from './lib/commands';
 
 const program = new Command();
 
-program.name('orchestr').description('A CLI builder for TypeScript');
+program
+  .name('Orchestr')
+  .description('Powerful JSON-based task automation for developers')
+  .usage('npx orchestr@latest <script-name>');
 
 // Default command (run script)
 program
   .command('run', { isDefault: true })
   .argument('[args...]', 'Catch all arguments/flags provided.')
   .allowUnknownOption()
-  .description('run the CLI')
+  .description('Run a script')
   .action(args => {
     // List command
     if (args[0] === '-l' || args[0] === '--list') {
@@ -19,6 +22,7 @@ program
       return;
     }
 
+    // Run command
     new CommandRun({ cmdArgs: args });
   });
 
@@ -27,7 +31,7 @@ program
   .command('validate')
   .argument('[args...]', 'Catch all arguments/flags provided.')
   .allowUnknownOption()
-  .description('validate the CLI')
+  .description('Validate a script')
   .action(args => {
     new CommandValidate({ cmdArgs: args });
   });
